77844d827f19ceb3155fe02db7eac9e24037605c
Ying Zhang <Winni731@my.yorku.ca>
2023-04-07 00:57:22 -0400

    simple chatbot

---
+++ src/components/utils.js
@@ -0,0 +1,22 @@
+import moment from "moment"
+export const analyze = (text) => {
+    if (text.includes('hi') || text.includes('hai') || text.includes('hello')) {
+        return "Hi, How can I help you?"
+    }
+    else if (text.includes('date')) {
+        return moment().format('MMMM Do YYYY')
+    }
+    else if (text.includes('time')) {
+        return moment().format('h:mm:ss a')
+    }
+    else if (text.includes('google link')) {
+        return "https://www.google.com"
+    }
+    else if (text.includes('interest')) {
+        return "Bank interest rate is 8.7"
+    }
+    else if (text.includes('thank you')) {
+        return "Thanks for Contacting me"
+    }
+    return "I cannot get you. Can you please rephrase the message?"
+}
