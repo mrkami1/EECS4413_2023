f420c59849383b78cb9497d2c4264fa882e81a99
mrkami1 <mashhood.anwar@gmail.com>
2023-04-08 16:30:38 -0400

    new cart and checkout ui

--- src/pages/view/Checkout.jsx
+++ src/pages/view/Checkout.jsx
@@ -6,6 +6,8 @@ import UserFieldsContext from "../../context/UserFieldsContext";
 import { db } from "../../firebase";
 import { uuidv4 } from "@firebase/util";
 import Navbar from "../../components/Navbar";
+import { Button, Card, CardContent, Paper, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 // for customer payment components
 function Checkout() {
@@ -14,12 +16,15 @@ function Checkout() {
     const [checkout, setCheckout] = useState([]);
     const [total, setTotal] = useState(0);
     const [error, setError] = useState(false);
+    const [expiry, setExpiry] = useState(false);
 
     const navigate = useNavigate();
 
     useEffect(() => {
         if (userFields) {
             setCheckout(userFields.cartItems);
+            const isExpired = userFields.payment.expiry < new Date().toISOString().split("T")[0];
+            setExpiry(isExpired);
         }
     }, [userFields]);
 
@@ -29,7 +34,7 @@ function Checkout() {
             totalCost += item.price * item.quantity;
         });
 
-        setTotal(totalCost);
+        setTotal((Math.round(totalCost * 100) / 100).toFixed(2));
     }, [checkout]);
 
     const order = async () => {
@@ -67,32 +72,86 @@ function Checkout() {
         });
     };
 
-    return (
-        <div>
-            <div>
-                <Navbar />
-            </div>
-            <div>
-                <p>Shipping address: {userFields?.address.address}</p>
-                <p>Payment method: {userFields?.payment.number}</p>
-                <p>Review items: </p>
-                <br />
-                {checkout.map((item) => {
+    const titleCard = () => (
+        <Card sx={{ mt: 5, ml: "auto", mr: "auto", maxWidth: "70%" }} raised>
+            <CardContent>
+                <Box sx={{ m: 2, borderBottom: "1px solid lightgray" }}>
+                    <Typography variant="h4">Shipping address</Typography>
+                    <Typography variant="h6" sx={{ wordBreak: "break-word", mb: 2 }}>
+                        {userFields?.address.address}
+                    </Typography>
+                </Box>
+                <Box sx={{ m: 2, mt: 4, borderBottom: "1px solid lightgray" }}>
+                    <Typography variant="h4">Payment method</Typography>
+                    <Typography variant="h6">{userFields?.payment.name}</Typography>
+                    <Typography variant="body1" sx={{ mb: 2 }}>
+                        Card ending in{" "}
+                        {userFields?.payment.number.substring(
+                            userFields?.payment.number.length - 4
+                        )}
+                    </Typography>
+                    {expiry && (
+                        <Typography variant="body1" sx={{ mb: 2 }} color="error">
+                            Card expired
+                        </Typography>
+                    )}
+                </Box>
+            </CardContent>
+        </Card>
+    );
+
+    const itemCard = () => (
+        <Card sx={{ mt: 5, ml: "auto", mr: "auto", maxWidth: "70%" }}>
+            <CardContent>
+                <Box sx={{ m: 2, mb: 4, borderBottom: "1px solid lightgray" }}>
+                    <Typography variant="h4">Review items</Typography>
+                </Box>
+                {checkout.map((item, i) => {
                     return (
-                        <div key={item.itemID}>
-                            <p>Item image: {item.image}</p>
-                            <p>Item name: {item.name}</p>
-                            <p>Item ID: {item.itemID}</p>
-                            <p>Price: {item.price}</p>
-                            <p>Quantity: {item.quantity}</p>
-                            <br />
-                        </div>
+                        <Paper
+                            key={i}
+                            elevation={3}
+                            sx={{
+                                margin: 2,
+                                display: "flex",
+                                flexDirection: "row",
+                                ":hover": { boxShadow: 10 },
+                                minHeight: 200,
+                            }}
+                        >
+                            <img src={item.image} style={{ margin: 20 }} />
+                            <Box sx={{ flex: "1", margin: 2 }}>
+                                <Typography variant="h4">{item.name}</Typography>
+                                <Typography variant="body1">Quantity : {item.quantity}</Typography>
+                                <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+                                    CAD ${item.price}
+                                </Typography>
+                            </Box>
+                        </Paper>
                     );
                 })}
-                <p>Order Total: {total}</p>
-                <button onClick={order}>Place your order</button>
-                {error && <p>Invalid shipping address or payment method</p>}
-            </div>
+                <Box sx={{ m: 2, mt: 4 }}>
+                    <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+                        TOTAL : CAD ${total}
+                    </Typography>
+                    {error &&
+                        <Typography color="error" variant="body1" sx={{ fontWeight: "bold" }}>
+                            Invalid payment method or shipping address
+                        </Typography>
+                    }
+                    <Button disabled={error} onClick={order} variant="contained" sx={{ mt: 2 }}>
+                        Place your order
+                    </Button>
+                </Box>
+            </CardContent>
+        </Card>
+    );
+
+    return (
+        <div>
+            <Navbar />
+            {titleCard()}
+            {itemCard()}
         </div>
     );
 }
